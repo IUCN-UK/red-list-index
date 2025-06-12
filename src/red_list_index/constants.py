3b1706ac5304550d95aa261a6f060e1e33747019
Ian Townsend <iant@users.noreply.github.com>
2025-06-12 14:44:17 +0100

    Update Red List categories (#3)

--- src/red_list_index/constants.py
+++ src/red_list_index/constants.py
@@ -1,13 +1,14 @@
 # Weights for each Red List category
 RED_LIST_CATEGORY_WEIGHTS = {
-    "LC": 0,
-    "NT": 1,
-    "VU": 2,
-    "EN": 3,
-    "CR": 4,
-    "CR(PE)": 5,
-    "CR(PEW)": 5,
-    "EW": 5,
-    "EX": 5,
-    "DD": None,
+    "LC": 0,      # Least Concern
+    "NT": 1,      # Near Threatened
+    "VU": 2,      # Vulnerable
+    "EN": 3,      # Endangered
+    "CR": 4,      # Critically Endangered
+    "RE": 5,      # Regionally Extinct
+    "CR(PE)": 5,  # Critically Endangered (Possibly Extinct)
+    "CR(PEW)": 5, # Critically Endangered (Possibly Extinct in the Wild)
+    "EW": 5,      # Extinct in the Wild
+    "EX": 5,      # Extinct
+    "DD": None    # Data Deficient
 }

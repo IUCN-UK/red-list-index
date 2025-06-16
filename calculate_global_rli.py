2791343fb784a6e6bdead21f0f781311e402cccf
Ian Townsend <ian.townsend@gmail.com>
2025-06-16 10:53:54 +0100

    Add some tests

--- calculate_global_rli.py
+++ calculate_global_rli.py
@@ -11,7 +11,11 @@ from pathlib import Path
 # Add 'src' directory to the module search path
 sys.path.append(str(Path(__file__).resolve().parent / "src"))
 
-from red_list_index.utils import validate_input_dataframe, add_weights_column
+from red_list_index.utils import (
+    validate_input_dataframe,
+    add_weights_column,
+    build_global_red_list_indices,
+)
 
 
 def main() -> None:
@@ -20,11 +24,18 @@ def main() -> None:
     )
     parser.add_argument("input_csv", help="Path to the input CSV file")
     parser.add_argument("output_csv", help="Path to save the output CSV file")
+    parser.add_argument(
+        "--number_of_repetitions",
+        type=int,
+        default=1000,
+        help="Number of repetitions (default: 1000)",
+    )
 
     args = parser.parse_args()
 
     input_file = args.input_csv
     output_file = args.output_csv
+    number_of_repetitions = args.number_of_repetitions
 
     try:
         df = pl.read_csv(input_file)
@@ -47,8 +58,27 @@ def main() -> None:
         print(f"[✗] Validating input DataFrame - {e}")
         sys.exit(1)
 
+    print(
+        f"\r[-] Building Global Red List Index DataFrame (number of repetitions: {number_of_repetitions})",
+        end="",
+        flush=True,
+    )
     try:
-        # TODO: Save CSV code to go here
+        rli_df = build_global_red_list_indices(df, number_of_repetitions)
+        print(
+            f"\r[✓] Building Global Red List Index DataFrame (number of repetitions: {number_of_repetitions}){' ' * 10}"
+        )
+    except Exception as e:
+        print(
+            f"\r[✗] Building Global Red List Index DataFrame (number of repetitions: {number_of_repetitions}) - {e}{' ' * 10}"
+        )
+        sys.exit(1)
+
+    try:
+        # Ensure 'group_sample_sizes' is cast to string for CSV output
+        rli_df = rli_df.with_columns(pl.col("group_sample_sizes").cast(pl.Utf8))
+        rli_df.write_csv(output_file)
+
         print(f"[✓] Saving results to: {output_file}")
     except Exception as e:
         print(f"[✗] Saving results to: {output_file} - {e}")

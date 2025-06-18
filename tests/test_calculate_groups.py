ca0f47c4ccf0f3453fa698ccb162c79e40960913
Ian Townsend <ian.townsend@gmail.com>
2025-06-18 11:21:37 +0100

    Assert the dataframe is correctly built

--- tests/test_calculate_groups.py
+++ tests/test_calculate_groups.py
@@ -7,27 +7,43 @@ def test_calculate_groups_initialization():
     # Create a sample DataFrame
     data = {
         "id": [1, 2, 3, 4],
-        "red_list_category": ["LC", "VU", "EN", "DD"],
+        "red_list_category": ["CR", "CR(PE)", "CR", "CR(PE)"],
         "year": [2020, 2020, 2021, 2021],
         "group": ["Mammals", "Mammals", "Birds", "Birds"],
-        "weights": [1.0, 0.8, 0.6, None],
+        "weights": [4, 5, 4, 5],
     }
     df = pl.DataFrame(data)
 
     # Initialize CalculateGroups
-    calculate_groups = CalculateGroups(df, number_of_repetitions=2)
+    calculate_groups = CalculateGroups(df, number_of_repetitions=1)
 
     # Assert the DataFrame is built correctly
     assert isinstance(calculate_groups.df, pl.DataFrame)
-    required_columns = [
-        "group",
-        "year",
-        "rli",
-        "qn_95",
-        "qn_05",
-        "n",
-        "group_sample_sizes",
-    ]
-    assert set(required_columns).issubset(calculate_groups.df.columns), (
-        f"Missing columns: {set(required_columns) - set(calculate_groups.df.columns)}"
+
+    expected_df = pl.DataFrame(
+        [
+            {
+                "group": "Birds",
+                "year": 2021,
+                "rli": 0.09999999999999998,
+                "qn_95": 0.09999999999999998,
+                "qn_05": 0.09999999999999998,
+                "n": 1,
+                "group_sample_sizes": {"group": "Birds", "count": 2},
+            },
+            {
+                "group": "Mammals",
+                "year": 2020,
+                "rli": 0.09999999999999998,
+                "qn_95": 0.09999999999999998,
+                "qn_05": 0.09999999999999998,
+                "n": 1,
+                "group_sample_sizes": {"group": "Mammals", "count": 2},
+            },
+        ]
     )
+
+    assert (
+        calculate_groups.df.sort("group").to_dicts()
+        == expected_df.sort("group").to_dicts()
+    ), "DataFrame does not match expected structure"

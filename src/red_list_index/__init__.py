784daa98435aa4c318763d297c161bddb42dcba6
Ian Townsend <ian.townsend@gmail.com>
2025-06-20 11:15:44 +0100

    Extract group year extrapolation functions

--- src/red_list_index/__init__.py
+++ src/red_list_index/__init__.py
@@ -2,5 +2,12 @@ from .calculate import Calculate
 from .data_frame_processor import DataFrameProcessor
 from .plot import Plot
 from .group_year_interpolation import GroupYearInterpolation
+from .group_year_extrapolation import GroupYearExtrapolation
 
-__all__ = ["Calculate", "DataFrameProcessor", "Plot", "GroupYearInterpolation"]
+__all__ = [
+    "Calculate",
+    "DataFrameProcessor",
+    "Plot",
+    "GroupYearInterpolation",
+    "GroupYearExtrapolation",
+]

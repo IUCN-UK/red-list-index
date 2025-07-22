64eb41ac6ac118118dbd825b4be481c2f5394c43
Ian Townsend <iant@users.noreply.github.com>
2025-07-22 13:23:39 +0100

    Update Red List Index calculator to use the dataclass decorator (#49)

--- src/red_list_index/calculate.py
+++ src/red_list_index/calculate.py
@@ -1,6 +1,9 @@
+from dataclasses import dataclass
+from typing import List
 from .constants import RED_LIST_CATEGORY_WEIGHTS
 
 
+@dataclass
 class Calculate:
     """
     Calculator for the Red List Index.
@@ -14,37 +17,35 @@ class Calculate:
         0.4
     """
 
-    def __init__(self, category_weights):
-        self.weight_of_extinct = RED_LIST_CATEGORY_WEIGHTS["EX"]
-        _validate_category_weights(category_weights, self.weight_of_extinct)
-
-        self.category_weights = category_weights
+    category_weights: List[int]
 
     def red_list_index(self):
         sum_of_weights = sum(self.category_weights)
-
         calculated_red_list_index = 1 - (
-            sum_of_weights / (self.weight_of_extinct * len(self.category_weights))
+            sum_of_weights
+            / (RED_LIST_CATEGORY_WEIGHTS["EX"] * len(self.category_weights))
         )
-
         return calculated_red_list_index
 
+    def __post_init__(self):
+        # __post_init__ is called automatically after the dataclass __init__ method.
+        # Here, we perform some validation on category_weights to ensure correctness before calculations.
 
-def _validate_category_weights(category_weights, weight_of_extinct):
-    if not category_weights:
-        raise ValueError("category_weights cannot be empty.")
-
-    for index, weight in enumerate(category_weights):
-        if weight is None:
-            raise ValueError(f"Null value found at index {index} in category_weights.")
-        if not isinstance(weight, int):
-            raise ValueError(
-                f"Non-integer value found at index {index}: {weight} ({type(weight).__name__})"
-            )
-        if weight < 0:
-            raise ValueError(f"Negative value found at index {index}: {weight}")
-        if weight > weight_of_extinct:
-            raise ValueError(
-                f"Value greater than EX found at index {index}: {weight} > {weight_of_extinct}"
-            )
-    return True
+        if not self.category_weights:
+            raise ValueError("category_weights cannot be empty.")
+        for index, weight in enumerate(self.category_weights):
+            if weight is None:
+                raise ValueError(
+                    f"Null value found at index {index} in category_weights."
+                )
+            if not isinstance(weight, int):
+                raise ValueError(
+                    f"Non-integer value found at index {index}: {weight} ({type(weight).__name__})"
+                )
+            if weight < 0:
+                raise ValueError(f"Negative value found at index {index}: {weight}")
+            if weight > RED_LIST_CATEGORY_WEIGHTS["EX"]:
+                raise ValueError(
+                    f"Value greater than EX found at index {index}: {weight} > {RED_LIST_CATEGORY_WEIGHTS['EX']}"
+                )
+        return True
